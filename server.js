6c23bf9a6412137fe7b085c1ddcfdb0fa2106f24
anvesh nani <anvesh nani>
2016-05-14 03:27:18 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+
+var app = express();
+
+app.listen(3000,function(err){
+	if(err){
+		console.log(err);
+	} else {
+		console.log("listening 3000");
+	}
+});
