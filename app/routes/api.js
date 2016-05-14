f6519338b57874311660120982c34919bd4d112e
phani05353 <phani05353@gmail.com>
2016-05-14 07:02:18 -0400

    login added

--- app/routes/api.js
+++ app/routes/api.js
@@ -5,6 +5,20 @@ var config = require('../../config');
 var secretKey = config.secretKey;
 
 
+var jsonwebtoken = require('jsonwebtoken');
+
+function createToken(user) {
+
+   var token = jsonwebtoken.sign({
+     id: user._id,
+     name: user.name,
+     username: user.username 
+   }, secretKey, {
+   
+   });
+
+   return token;
+}
 module.exports = function(app, express) {
 
 	var api = express.Router();
@@ -39,6 +53,43 @@ module.exports = function(app, express) {
 			res.json(users);
 		});
 	});
+
+  api.post('/login',function(req, res){
+
+      User.findOne({ 
+         username: req.body.username
+
+      }).select('password').exec(function(err, user){
+          
+          if(err) throw err;
+          if(!user) {
+
+          	res.send({ message: "User Not Defined"});
+
+          } else if(user){
+          	var validPassword = user.comparePassword(req.body.password);
+
+          	if (!validPassword){
+          		res.send({ message: "Invalid Password"});
+          	} else{
+                 //token
+
+                 var token = createToken(user);
+
+                 res.json({
+
+                 	success:true,
+                 	message: "Login Successful!!",
+                 	token: token
+                 });
+            }
+
+          }
+     });
+
+  });
+
+
 
 return api
 
